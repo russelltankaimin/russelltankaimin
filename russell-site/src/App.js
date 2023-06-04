70a2e62f00586b592a9f0e2b5536b9f215113083
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-06-04 15:07:10 +0800

    Fix router issues

--- russell-site/src/App.js
+++ russell-site/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import NavHeader from './components/NavHeader';
 import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Consulting from './components/pages/Consulting';
+import { router } from './router';
 
 function App() {
   return (
-    <Router>
-      <NavHeader />
-      <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/consulting' component={Consulting} />
-      </Routes>
-    </Router>
+    <>
+    <NavHeader />
+    {router}
+    </>
   );
 }
 
--- russell-site/src/components/NavHeader.jsx
+++ russell-site/src/components/NavHeader.jsx
@@ -1,15 +1,14 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link } from 'react-router-dom';
 
 function OffcanvasExample() {
 
-  const expand = 'lg'
+  const expand = false
 
   return (
     <>
@@ -19,30 +18,29 @@ function OffcanvasExample() {
         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Contents
-                </Offcanvas.Title>
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Contents
+              </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Skills</Nav.Link>
+                  <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                  <Nav.Link as={Link} to='/skills'>Skills</Nav.Link> 
                   <Nav.Link href="#action2">Software/Projects</Nav.Link>
                   <Nav.Link href="#action2">About</Nav.Link>
                   <NavDropdown
                     title="Blog"
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
-                    <NavDropdown.Item href="#action3">
+                  <NavDropdown.Item href="#action3">
                     University</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action4">
-                      Interests
-                    </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action4">
+                    Interests
+                  </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
               </Offcanvas.Body>
