4cc049ac744071ff9b2fb6b2ad92a753e1b6b796
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-06-17 17:15:47 +0800

    Add software projects

--- src/components/pages/software/ProjectCard.jsx
+++ src/components/pages/software/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Card, Badge} from "react-bootstrap";
+import ModalProject from "./ModalProject";
 
 export const ProjectCard = ({
     project: {
@@ -13,21 +13,27 @@ export const ProjectCard = ({
     },
   }) => {
     return (
-    <Card style={{ width: '18rem', margin : "10px 30px 10px 30px" }}>
+    <Card style={{ width: '18rem', margin : "10px 30px 10px 30px", borderColor:"orange", borderWidth:"10px"}}>
     <Card.Body>
-        <Card.Title>{title}{status}</Card.Title>
+        <Card.Title>{title}
+        <Badge bg={status === 'Pending' ? "danger" : (status === "Completed" ? "success" : "secondary")}
+        style={{marginLeft : "10px", marginRight:"10px"}}>{status}</Badge>
+        </Card.Title>
         <Card.Text>
-          {description}
+        {timeline}
         </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        {Object.keys(links).map(x => <ListGroup.Item>
-        {x}
-        </ListGroup.Item>)}
-      </ListGroup>
-      <Card.Body>
-        {main}
-      </Card.Body>
+        <Card.Text>
+        <em>{description}</em>
+        <hr></hr>
+        <ModalProject title={title} main={main} tags={category}/>
+        </Card.Text>
+        <hr></hr>
+        <Card.Text>
+          {Object.keys(links).map(x => (<Card.Link href={links[x]} style={{color:"black"}}>{x}</Card.Link>))}
+          {category.length > 0 ? <hr></hr> : <></>}
+            {category.map(x => <Badge bg="dark" style={{margin:"3px 1px 3px 3px"}} text="light">{x}</Badge>)}
+        </Card.Text>
+    </Card.Body>
     </Card>
   
     );
--- src/components/pages/software/SoftwarePage.jsx
+++ src/components/pages/software/SoftwarePage.jsx
@@ -3,6 +3,7 @@ import { PageImage } from "../PageImage";
 import picture from "../images/projects.png"
 import { ProjectCard } from "./ProjectCard";
 import data from "./projects.json";
+import { Row } from "react-bootstrap";
 
 export const SoftwarePage = () => {
     return (
@@ -12,8 +13,15 @@ export const SoftwarePage = () => {
         I have done some projects in school and my own spare time. I do them usually because of something that I want to experiment with, have fun and learn from.
         Projects started after May 2023 are shifted to my Github Organisation : russell-project-org.
         </p>
-        {console.log(data["in_progress"])}
+        <h3 style={{color:"white", marginLeft:"40px"}}>In Progress / Planned</h3>
+        <hr style={{color:"white"}}></hr>
+        <Row xs={1} md={2} lg={4} xl={4} xxl={4} className="g-1">
         {data["in_progress"].map(proj => <ProjectCard project={proj}/>)}
+        </Row>
+        <h3 style={{color:"white", marginLeft:"40px"}}>Past Projects</h3>
+        <hr style={{color:"white"}}></hr>
+        <Row xs={1} md={2} lg={4} xl={4} xxl={4} className="g-1">
+        {data["completed"].map(proj => <ProjectCard project={proj}/>)}</Row>
         </>
     );
 }
